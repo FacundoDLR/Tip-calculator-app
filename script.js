0233126a83a513c7e979d51f0a9e0c77f830a3e6
Facundo DE LOS RIOS <facujofre96@gmail.com>
2024-12-15 23:39:49 +0100

    fix: added validations of negative numbers in form entries

--- script.js
+++ script.js
@@ -10,45 +10,66 @@ const resetBtn = document.getElementById("resetBtn");
 let selectedTip = 0;
 let customTip = 0;
 
-const getBillAmount = () => parseFloat(billAmountInput.value) || 0;
-const getNumPeople = () => parseInt(numPeopleInput.value) || 1;
+const getBillAmount = () => {
+    const value = parseFloat(billAmountInput.value) || 0;
+    return value >= 0 ? value : 0; // If the value is negative, we convert it to 0
+}
+const getNumPeople = () => {
+    const value = parseInt(numPeopleInput.value) || 1;
+    return value >= 1 ? value : 1; // If the number of people is less than 1, we convert it to 1
+}
 
 tipButtons.forEach(button => {
     button.addEventListener('click', () => {
         tipButtons.forEach(btn => btn.classList.remove('active'));
         button.classList.add('active');
         selectedTip = parseFloat(button.dataset.tip);
+        customTipInput.value = '';
+        customTip = 0;
         console.log('selected tip: ' + selectedTip);
         calculate(); // recalculate with selctedTip
-
     });
 })
 
-customTipInput.addEventListener('input', function() {
+customTipInput.addEventListener('input', function () {
     customTip = parseFloat(customTipInput.value) || 0; // get custom tip value
+    if (customTip < 0) {
+        customTip = 0;
+        customTipInput.value = '0';
+    }
+
+    if (customTip === 0) {
+        selectedTip = 0; // Reset selected tip
+        tipButtons.forEach(btn => btn.classList.remove('active')); // Uncheck all buttons
+    }
     console.log('Custom tip: ' + customTip);
     calculate(); // recalculate with custom tip
 })
 
+billAmountInput.addEventListener('input', () => {
+    calculate();
+})
+
+numPeopleInput.addEventListener('input', () => {
+    calculate();
+})
 
 const calculate = () => {
-    
+
     const billAmount = getBillAmount(); // Obtain total amount
     const numPeople = getNumPeople(); // Obtain N° people
 
     // Total without Tip
     const totalPerPerson = billAmount / numPeople;
-    totalAmountPerPerson.textContent = `${totalPerPerson.toFixed(2)}`;
+    if (!customTip && customTip === 0) {
+        totalAmountPerPerson.textContent = `${totalPerPerson.toFixed(2)}`;
 
-    if(customTip === 0 && selectedTip === 0){
-        console.log('No tip has been selected.')
-        return;
     }
 
     let tipAmount = 0;
-    if(customTip > 0){
-        tipAmount = customTip;
-    }else if (selectedTip > 0 ) {
+    if (customTip > 0) {
+        tipAmount = (billAmount * customTip) / 100;
+    } else if (selectedTip > 0) {
         tipAmount = (billAmount * selectedTip) / 100;
     }
     // Tip per Person
